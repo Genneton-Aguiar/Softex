b40fe3d54b5653a04115a659fad11ea991833bcb
Genneton Aguiar <140842169+Genneton-Aguiar@users.noreply.github.com>
2023-09-03 02:48:37 -0300

    Add files via upload

---
+++ Exercicio_09.js
@@ -0,0 +1,16 @@
+/* Declare as seguintes variáveis: nome, idade, altura e maior_de_idade.
+
+Depois, atribua valores para essas variáveis, sabendo que a variável nome é uma string, 
+a variável idade é um inteiro, a variável altura é um float e a variável maior_de_idade é um booleano.
+
+No final, imprima os valores das variáveis. */
+
+let nome = "Genneton Victor"
+let idade = 23
+let altura = parseFloat(1.80);
+let maiorDeIdade = true;
+
+console.log("Este é um tipo STRING: " + nome );
+console.log("Este é um tipo INTI: "+ idade);
+console.log("Este é um tipo FLOAT: " + altura.toFixed(2));
+console.log("Este é um tipo BOOLEANO: "+ maiorDeIdade);
