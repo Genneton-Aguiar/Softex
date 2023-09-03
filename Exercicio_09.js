56fe3974eb1a070028ff7f7d1b14e6a5fe47e627
Genneton Aguiar <140842169+Genneton-Aguiar@users.noreply.github.com>
2023-09-03 02:46:11 -0300

    Add files via upload

---
+++ Exercicio_09.js
@@ -0,0 +1,9 @@
+let nome = "Genneton Victor"
+let idade = 23
+let altura = parseFloat(1.80);
+let maiorDeIdade = true;
+
+console.log("Este é um tipo STRING: " + nome );
+console.log("Este é um tipo INTI: "+ idade);
+console.log("Este é um tipo FLOAT: " + altura.toFixed(2));
+console.log("Este é um tipo BOOLEANO: "+ maiorDeIdade);
