3cc4338134ad12565e17e726e5f4d09a171f196b
Genneton Aguiar <140842169+Genneton-Aguiar@users.noreply.github.com>
2023-09-05 04:31:40 -0300

    Add files via upload

---
+++ Exercicio_12.js
@@ -0,0 +1,14 @@
+/*Faça um código que execute a contagem regressiva de uma bomba, 
+informando o número de segundos para explodir. 
+Ele deverá mostrar a mensagem “iniciando contagem regressiva”, os segundos passados e, no final, 
+a mensagem “BUM!”.*/
+
+console.log("Iniciando contagem regressiva para explosão:");
+var i=5;
+do{
+  console.log(i);
+  i=i-1;
+}
+  while(i){
+  console.log("KABUMM!!")
+  }
